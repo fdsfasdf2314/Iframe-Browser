3d883a42ebbe93bb557ebc1875c2dc9ec03e72d0
James <123058@combertonvc.org>
2025-01-10 13:20:21 +0000

    Create searchp.js

---
+++ searchp.js
@@ -0,0 +1,14 @@
+// Get the current URL's query string (everything after '?')
+const queryString = window.location.search;
+const urlInput = document.getElementById("urlInput");
+
+// Create a URLSearchParams object from the query string
+const urlParams = new URLSearchParams(queryString);
+
+// Access specific query parameters by name
+var urlParam = urlParams.get('url');
+
+
+// Load the iFrame
+urlInput.textContent = urlParam;
+loadIframe();
